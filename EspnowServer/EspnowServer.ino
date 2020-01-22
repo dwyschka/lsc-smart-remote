a7fd0e567cf821c522008ce15ed384580fd626a1
Daniel Wyschka <d.wyschka@sensou.de>
2020-01-22 19:35:57 +0100

    Remove comments

--- EspnowServer/EspnowServer.ino
+++ EspnowServer/EspnowServer.ino
@@ -1,23 +1,3 @@
-/*
- Testing ESP-Now 
- See https://espressif.com/en/products/software/esp-now/overview
- ESP-Now enables fast lightwieght connectionless communication between ESP8266's.
- So for example a remote sensor node could send a reading using ESP-Now in just a few 
- hundred milliseconds and deepSleep the rest of the time and so get increased battery 
- life compared to the node using a regular WiFi connection which could take 10 times 
- as long to connect to WiFi and send a sensor reading.
- 
- ESP-Now has the concept of controllers and slaves. AFAICT the controller is the remote
- sensor node and the slave is the always on "gateway" node that listens for sensor node readings. 
- *** Note: to compile ESP-Now (with arduino/esp8266 release 2.3.0) need to edit 
- * ~/Library/Arduino15/packages/esp8266/hardware/esp8266/2.1.0/platform.txt 
- * Search "compiler.c.elf.libs", and append "-lespnow" at the end of the line. 
- * See: http://www.esp8266.com/viewtopic.php?p=44161#p44161 
- ***
- **** This skecth is the slave/gateway node ****
- Ant Elder
- License: Apache License v2
-*/
 #include <ESP8266WiFi.h>
 extern "C" {
   #include <stdint.h>
@@ -37,10 +17,6 @@ void setup() {
 
   Serial.print("This node AP mac: "); Serial.print(WiFi.softAPmacAddress());
   Serial.print(", STA mac: "); Serial.println(WiFi.macAddress());
-
-  // Note: When ESP8266 is in soft-AP+station mode, this will communicate through station interface
-  // if it is in slave role, and communicate through soft-AP interface if it is in controller role,
-  // so you need to make sure the remote nodes use the correct MAC address being used by this gateway. 
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
 
   esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len) {
--- LSCSmartRemote/LSCSmartRemote.ino
+++ LSCSmartRemote/LSCSmartRemote.ino
@@ -12,19 +12,17 @@ uint8_t remoteMac[] = {0x6A, 0xC6, 0x3A, 0xC2, 0xFE, 0x7A};
 #define WIFI_CHANNEL 1
 #define SEND_TIMEOUT 500
 
-//is OFF
-
 void setup() {
   Serial.begin(9600);
 
-  WiFi.mode(WIFI_STA); // Station mode for sensor/controller node
+  WiFi.mode(WIFI_STA);
   WiFi.begin();
 
   if (esp_now_init()!=0) {
     ESP.restart();
   }
 
-  delay(1); // This delay seems to make it work more reliably???
+  delay(1); 
 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
@@ -33,8 +31,6 @@ void setup() {
       Serial.print(0x00);
   });
 }
-
-//    esp_now_send(NULL, data2send, sizeof(data2send));
 
 void loop() {
   if(Serial.available() > 0) {
